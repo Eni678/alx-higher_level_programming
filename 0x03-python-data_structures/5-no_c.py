5d6d27526a993308a97bff274249c0c22deabd88
Eni678 <enilamaoshoriamhe687@gmail.com>
2024-02-26 13:24:00 +0100

    none

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if not my_string:
-        return ("")
-    else:
-        result = ""
-        for char in my_string:
-            if char != 'c' and char != 'C':
-                result += char
-                return (result)
+    new_str = ""
+    for i in my_string:
+        if i not int "cC":
+            new_str += 1
+            return (new_str)
