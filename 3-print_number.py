6a5953e261f3d1a4f9a8d2f464800c613adabf07
Eni678 <enilamaoshoraimhe687@gmail.com>
2024-03-06 09:52:01 +0100

    a

--- 3-print_number.py
+++ 3-print_number.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 number = 98
+<<<<<<< HEAD
 print(f"{number} Battery street")
 print()
+=======
+print(f"{number:d} Battery street")
+>>>>>>> 3bde63b7208b8ab7f061a84b5322d7bf7f30ecfa
